89c0fca1cf92ac7fc085fefdfbea35af777f6156
Simone Björklund <simone_1000@hotmail.com>
2018-11-08 16:00:26 +0100

    Added hosting

---
+++ WCFGameLibrary.SelfHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+using WCFGameLibrary.Services;
+using System;
+using System.Diagnostics;
+
+namespace WCFGameLibrary.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost host = new ServiceHost(typeof(WCFGameLibraryService));
+                host.Open();
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+                host.Close();
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
